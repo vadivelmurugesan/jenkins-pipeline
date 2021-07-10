800b4345a68bd27c1e3cd1e82041227d3c3a85cf
vadivel <vadivelm85@gmail.com>
2021-07-10 09:05:10 -0400

    Add spring project for demo purpose

---
+++ src/main/java/engineer/vel/jenkins/jenkinspipeline/JenkinsPipelineApplication.java
@@ -0,0 +1,13 @@
+package engineer.vel.jenkins.jenkinspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/engineer/vel/jenkins/jenkinspipeline/JenkinsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package engineer.vel.jenkins.jenkinspipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
